a5dd570f4b8bcfc31e7e3417d9d60aa20780e4d7
Oz Akan <oz@akan.me>
2018-01-03 21:38:16 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Index
+
+### Hello World
+
+[hello_world.cpp][hello_world]
+
+
+[hello_world]: hello_world.cpp
---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    // print "Hello World"
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
