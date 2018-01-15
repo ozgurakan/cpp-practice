8450777d16de4feb24faf6184135c9def0068c7e
Oz Akan <oz@akan.me>
2018-01-14 22:55:11 -0500

    class example

---
+++ 1036_classes01.cpp
@@ -0,0 +1,34 @@
+/*
+classes
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human
+{
+public:
+    string name;
+    int age;
+
+    void Hello()
+    {
+        cout << "I am " + name << ", I am ";
+        cout << age << " years old." << endl;
+    }
+};
+
+int main(void)
+{
+    Human human;
+    human.age = 22;
+    human.name = "Superman";
+    human.Hello();
+
+    Human* human2 = new Human();
+    human2->age = 11;
+    human2->name = "AI";
+    human2->Hello();
+
+    return 0;
+}
