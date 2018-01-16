ecb736c89898540000bc21c28f2adf2efdf8891b
Oz Akan <oz@akan.me>
2018-01-15 20:37:28 -0600

    constructor and scope resolution

--- 1037_classes02.cpp
+++ 1037_classes02.cpp
@@ -13,6 +13,12 @@ private:
     int age;
 
 public:
+    Animal()
+    {
+        age = 0;
+        species = "Human";
+    }
+
     void SetAge(int inputAge)
     {
         age = inputAge;
@@ -22,7 +28,21 @@ public:
     {
         return age;
     }
+
+    void SetSpecies(string);
+
+    string GetSpecies()
+    {
+        return species;
+    }
 };
+
+void Animal::SetSpecies(string name)
+{
+    if (name != species)
+        cout << "Converting " << species << " to " << name << endl;
+    species = name;
+}
 
 int main(void)
 {
@@ -30,7 +50,11 @@ int main(void)
     
     Animal* person = new Animal();
     person->SetAge(11);
+    //person->age = 12; // wouldn't work
     cout << "Age: " << person->GetAge() << endl;
+    cout << "Species: " << person->GetSpecies() << endl;
+    person->SetSpecies("Donkey");
+    cout << "Species: " << person->GetSpecies() << endl;
 
     delete person;
 
