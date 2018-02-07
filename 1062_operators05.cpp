cc8a08b681595b46274e192f6b1b0a0161923cb0
Oz Akan <oz@akan.me>
2018-02-06 22:15:24 -0500

    equality operator

---
+++ 1062_operators05.cpp
@@ -0,0 +1,53 @@
+/*
+equality operator
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human
+{
+public:
+    string name;
+
+    Human(string inName): name(inName) {}
+
+    bool operator == (const Human& inHuman)
+    {
+        if (name.compare(inHuman.name) == 0)
+            return true;
+        else
+            return false;        
+    }
+};
+
+class Animal
+{
+public:
+    string name;
+
+    Animal(string inName): name(inName) {}
+};
+
+int main(void) 
+{
+    Human joe("joe");
+    Human marry("joe");
+
+    if (joe == marry)
+        cout << "Same names" << endl;
+    else
+        cout << "Different names" << endl;
+
+    Animal a1("joe");
+    Animal a2("joe");
+
+    if (a1 == a2) // error: invalid operands to binary expression
+        cout << "Same names" << endl;
+    else
+        cout << "Different names" << endl;
+
+
+
+    return 0;
+}
